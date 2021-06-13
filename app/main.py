4758e6308bde19dde884b926e902384dbc05080a
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2021-06-13 10:29:53 +0800

    Changing Screen
    Successfully changing the screen using button and ScreenManager

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
+from logging import root
 import kivy
 kivy.require('2.0.0')
 
 from kivy.app import App
 from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.button import Button
-# from kivy.uix.label import Label
+from kivy.uix.label import Label
 # from kivy.uix.recycleview import RecycleView
 # from kivy.uix.floatlayout import FloatLayout
-# from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
 from kivy.graphics import Color, Rectangle
+from kivy.properties import StringProperty
 from kivy.core.image import Image
 from kivy.core.window import Window
 
@@ -20,60 +22,31 @@ Config.set('graphics', 'width', 400)
 Config.set('graphics', 'height', 600)
 Config.write()
 
-## Label
-
-## class FirstKivy(App):
-##    def build(self):
-##        return Label(text='[u][color=ff0066][b]Better days[/b][/color] are coming; they are called [i][color=ff9933]Saturday[/i] and [i]Sunday[/i][/color][/i][/u]', markup= True)
-
-## Button
-
-## class KivyButton(App):
-##    def build(self):
-##        return Button(text="Test", pos=(300,350), size_hint=(.25, .18))
-
-## FirstKivy().run()
-## KivyButton().run()
-
-## RecycleView
-
-# Builder.load_string('''
-
-# <ExampleRV>:
-#     viewclass: 'Button'
-
-#     RecycleBoxLayout:
-
-#         size_hint_y: None
-
-#         height: self.minimum_height
-
-#         orientation: 'vertical'
-
-# ''')
-
-# class ExampleRV(RecycleView):
+# class Game(BoxLayout):
+#     label_text = StringProperty()
 #     def __init__(self, **kwargs):
-#         super(ExampleRV, self).__init__(**kwargs)
-#         self.data = [{'text': str(x)} for (x) in range(20)]
+#         super(Game, self).__init__(**kwargs)
+#         self.count = 0
+#         self.label_text = str(self.count)
 
-# class RecycleApp(App):
-#     def build(self):
-#         return ExampleRV()
+#     def increment(self, *args):
+#         self.count += 1
+#         self.label_text = str(self.count)
+#         print (self.label_text)
 
-# RecycleApp().run()
-
+# StartScreen
 class StartScreen(Screen):
     pass
 
+# GameScreen
 class GameScreen(Screen):
     pass
 
+# RootScreen
 class RootScreen(ScreenManager):
     pass
 
-# Main App
-
+# Main
 class MainApp(App):
     def build(self):
         return RootScreen()

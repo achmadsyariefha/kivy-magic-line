e5f5cd7e1a44480c5156513f33f21c7dc5d714be
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2021-11-12 13:38:38 +0800

    add sound toggle
    added sound toggle

--- app/main.py
+++ app/main.py
@@ -7,45 +7,36 @@ from kivymd.uix import button
 from kivymd.uix.behaviors import backgroundcolor_behavior
 kivy.require('1.0.0')
 
-from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDFillRoundFlatButton
-from kivymd.uix.button import MDFlatButton
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.label import MDLabel
-from kivymd.uix.menu import MDDropdownMenu
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.gridlayout import MDGridLayout
-from kivymd.uix.selectioncontrol import MDCheckbox
 from kivy.config import Config
 from kivy.clock import Clock
 from kivy.core.image import Image
 from kivy.core.window import Window
-from kivy.core.audio import SoundLoader
-from kivy.lang.builder import Builder
-from kivy.properties import ObjectProperty
-from kivy.properties import NumericProperty
-from kivy.properties import StringProperty
+from kivy.core.audio import SoundLoader, Sound
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty, BooleanProperty
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDFillRoundFlatButton, MDFlatButton, MDRectangleFlatButton
+from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.boxlayout import MDBoxLayout
 
-## Window Configuration
-
-Config.set('graphics', 'width', 400)
-Config.set('graphics', 'height', 600)
-Config.set('input', 'mouse', 'mouse,disable_multitouch')
-Config.write()
-Window.size = (400,600)
-Window.borderless = True
-Clock.max_iteration = 20
+sound = ObjectProperty(None, allownone=True)
 
 ## Background Music
+class BackgroundMusic(Sound):
+    def __init__(self):
+        self.sound = SoundLoader.load('resources/bgm/beach_party.mp3')
 
-sound = SoundLoader.load('resources/bgm/beach_party.mp3')
-# sound.play()
+## Sound Effects
+class SoundEffects(Sound):
+    def __init__(self):
+        self.sound = SoundLoader.load('resources/sfx/sample.mp3')
 
 # StartScreen
 class StartScreen(MDScreen):
@@ -94,16 +85,19 @@ class GameScreen(MDScreen):
 
 # SettingsScreen
 class SettingsScreen(MDScreen):
-    soundVolume = NumericProperty(0.0)
+    def __init__(self, **kwargs):
+        super(SettingsScreen, self).__init__(**kwargs)
+        self.bgm_toggle = SoundToggle()
 
     def set_screen(self):
         MDApp.get_running_app().root.current = "start"
         MDApp.get_running_app().root.transition.direction = "right"
 
-    def on_bgm_slider_value(self, widget):
-        soundVolume = float(widget.value/100)
-        sound.volume = soundVolume
-        # print("Slider : "+ str(widget.value/100))
+    def toggle_bgm(self, instance, value):
+        if value:
+            print('Switch is', instance.state, 'state')
+        else:
+            print('Switch is', instance.state, 'state')
 
     def on_sfx_slider_value(self, widget):
         # soundVolume = float(widget.value/100)
@@ -125,13 +119,36 @@ class HelpScreen(MDScreen):
         MDApp.get_running_app().root.current = "start"
         MDApp.get_running_app().root.transition.direction = "right"
         
-
 # RootScreen
 class RootScreen(ScreenManager):
     pass
 
-# GameBall
+# Toggle Sound
+class SoundToggle(MDRectangleFlatButton, MDToggleButton):
+    state = StringProperty(None)
+    active = BooleanProperty(False)
+    sound = ObjectProperty(None, allownone=True)
+    def __init__(self, **kwargs):
+        super(SoundToggle, self).__init__(**kwargs)
+        self.background_down = (0, 0, 1, 1)
+        self.font_color_down = [1, 0, 0, 1]
+        self.font_size = "12sp"
+        self.allow_no_selection = False
+        self.sound = BackgroundMusic()
+        self.bind(state = self.bgm_on_state)
+        self.sfx = SoundEffects()
 
+    def bgm_on_state(self, instance, value):
+        if self.state == 'down':
+            self.sound.play()
+        else:
+            self.sound.stop()
+
+    def sfx_on_state(self, widget, value):
+        if value == 'down':
+            self.sfx.play()
+        else:
+            self.sfx.stop()
 
 # Main
 class MainApp(MDApp):
@@ -139,6 +156,14 @@ class MainApp(MDApp):
     dialog = None
     
     def build(self):
+        Config.set('graphics', 'width', 400)
+        Config.set('graphics', 'height', 600)
+        Config.set('input', 'mouse', 'mouse,disable_multitouch')
+        Config.write()
+        Window.size = (400,600)
+        Window.borderless = True
+        Clock.max_iteration = 20
+        self.title = "Magic Line"
         return RootScreen()
 
     def show_exit_dialog(self):
@@ -165,11 +190,13 @@ class Player(object):
     name = ''
     score = 0
     time = 0
+    turn = 0
 
-    def __init__(self, name, score, time):
+    def __init__(self, name, score, time, turn):
         self.name = name
         self.score = score
         self.time = time
+        self.turn = turn
         return
 
 # GameBall
@@ -185,6 +212,16 @@ class GameBall(object):
         self.rows = rows
         self.column = column
         return
+
+class GameBoard(Widget):
+    def __init__(self, **kwargs):
+        super(GameBoard, self).__init__(**kwargs)
+
+class GameBoardColumn(Widget):
+    def __init__(self, **kwargs):
+        super(GameBoardColumn, self).__init__(**kwargs)
+        Window.bind(mouse_pos=self.on_mouse_pos)
+        self.magicLineGame = None
 
 if __name__ =="__main__":
     MainApp().run()
